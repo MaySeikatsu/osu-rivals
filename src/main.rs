4da1e72af7f3a3da57984106822ee068d8f9cc7f
maike <maynoshinseikatsu@gmail.com>
2025-08-10 13:04:14 +0200

    chore

--- src/main.rs
+++ src/main.rs
@@ -8,50 +8,9 @@ mod ratatui_test;
 
 #[tokio::main]
 async fn main() {
-    
     // api_v2::v2().await;
     // v2().await;
 
     // let _ = rank::v2().await;
     ratatui_test::basic_tui();
-}
-
-// #[tokio::main]
-pub async fn v2() {
-    // Initilize Client
-    let client_id: u64 = 42909;
-    let client_secret = String::from("rBP4sRzwcGL9bYiqLb5fX1UXDuwtrY7LwWO8oJSh");
-    let osu = Osu::new(client_id, client_secret).await.unwrap();
-
-    // Get peppy's top 10-15 scores in osu!standard.
-    // Note that the username here can only be used because of the `cache` feature.
-    // If you are fine with just providing user ids, consider disabling this feature.
-    println!("You should now see your score:");
-    let scores: Vec<Score> = osu
-        .user_scores("mayseikatsu")
-        .mode(GameMode::Osu)
-        .best() // top scores; alternatively .recent(), .pinned(), or .firsts()
-        .offset(00)
-        .limit(1)
-        .await
-        .unwrap();
-    println!("{:#?}", scores);
-
-    // Search non-nsfw loved mania maps matching the given query.
-    // Note that the order of called methods doesn't matter for any endpoint.
-    // let search_result: BeatmapsetSearchResult = osu.beatmapset_search()
-    //     .nsfw(false)
-    //     .status(Some(RankStatus::Loved))
-    //     .mode(GameMode::Mania)
-    //     .query("blue army stars>3")
-    //     .await
-    //     .unwrap();
-    // println!("{:#?}",search_result);
-
-    // Get the french wiki page on the osu file format
-    // let wiki_page: WikiPage = osu.wiki("fr")
-    //     .page("Client/File_formats/osu_%28file_format%29")
-    //     .await
-    //     .unwrap();
-    // println!("{:#?}",wiki_page);
 }
